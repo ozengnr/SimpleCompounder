deed7dd4ef4dc65c3eb8e987f4cabe258fa4e069
Oz <ozengnr@gmail.com>
2019-07-07 11:22:04 -0400

    Add files via upload

---
+++ SimpleCompounder.cpp
@@ -0,0 +1,36 @@
+/* This is a simple compound interest calculator. */
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    double amountYearEnd;
+    double presentValue;
+    double discountRate;
+    int yearInput;
+
+    //set floating-point number format
+    cout << fixed << setprecision(2);
+
+    //user inputs
+    cout << "Please enter the number of years: ";
+    cin >> yearInput;
+
+    cout << "Please enter the present value / principal: ";
+    cin >> presentValue;
+
+    cout << "Please enter the discount rate as a decimal: ";
+    cin >> discountRate;
+
+    //display headers
+    cout << "Year" << setw(22) << "End amount" << endl;
+    
+    //calculate
+    for (int year = 0; year <= yearInput ; year++)
+    {
+        amountYearEnd = presentValue * pow(1.0 + discountRate, year);
+        cout << setw(4) << year << setw(22) << amountYearEnd << endl;
+    }
+}
